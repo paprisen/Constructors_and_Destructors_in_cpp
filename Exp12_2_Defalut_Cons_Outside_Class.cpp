57226d3eebd0ce8885de0bb2db2a00d3f0f93c48
Papri <papri.sen2020@gmail.com>
2025-08-30 11:07:05 +0530

    Update Exp12_2_Defalut_Cons_Outside_Class.cpp

--- Exp12_2_Defalut_Cons_Outside_Class.cpp
+++ Exp12_2_Defalut_Cons_Outside_Class.cpp
@@ -1,65 +1,40 @@
 // Papri Sen
 // 24070123070
 // A3
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-#include<iostream>
-using namespace std;
+class Date {
+    int day, month, year;
 
-class Marks{
-    public:
-
-    int SS;
-    int EDC;
-    int DCLD;
-    int NT;
-    int MTT;
-
-    Marks();
+public:
+    Date();        
     void display();
 };
 
-Marks::Marks(){
-     cout<<"Enter SS marks:";
-        cin>>SS;
 
-        cout<<"Enter EDC marks:";
-        cin>>EDC;
-
-        cout<<"Enter DCLD marks:";
-        cin>>DCLD;
-
-        cout<<"Enter NT marks:";
-        cin>>NT;
-
-        cout<<"Enter MTT marks:";
-        cin>>MTT;
-        
+Date::Date() {
+    cout << "Enter day: ";
+    cin >> day;
+    cout << "Enter month: ";
+    cin >> month;
+    cout << "Enter year: ";
+    cin >> year;
 }
 
-void Marks::display(){
-    cout<<endl<<"SS: "<<SS<<"\n"<<"EDC: "<<EDC<<"\n"<<"DCLD: "<<DCLD<<"\n"<<"NT: "<<NT<<"\n"<<"MTT: "<<MTT;
+void Date::display() {
+    cout << "Date: " << day << "-" << month << "-" << year << endl;
 }
 
-int main(){
-
-    Marks m;
-    m.display();
-
-    return 0;
+int main() {
+    Date d;     
+    d.display(); 
 }
+
 /*
 Output:
-Enter SS marks:78
-Enter EDC marks:74
-Enter DCLD marks:83
-Enter NT marks:92
-Enter MTT marks:89
-
-SS: 78
-EDC: 74
-DCLD: 83
-NT: 92
-MTT: 89
+Enter day: 7
+Enter month: 8
+Enter year: 2025
+Date: 7-8-2025
 */
