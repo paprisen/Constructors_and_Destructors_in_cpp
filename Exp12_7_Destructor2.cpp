b5e52a11c7745f681a870ad262e9c0855332caf1
Papri <papri.sen2020@gmail.com>
2025-08-31 11:22:34 +0530

    Create Exp12_7_Destructor2.cpp

---
+++ Exp12_7_Destructor2.cpp
@@ -0,0 +1,34 @@
+// Papri Sen
+// 24070123070
+// A3
+#include <iostream>
+using namespace std;
+class Date{
+    public:
+    int d=28;
+    int m=8;
+    int y=2025;
+    
+    ~Date(){
+        cout<<"Destructor called"<<endl;
+    }
+};
+int main() {
+    Date d1,d2,d3,d4;
+    int i;
+    for(i=0;i<4;i++){
+        Date d1;
+    }
+}
+
+/*
+Output:
+Destructor Called
+Destructor Called
+Destructor Called
+Destructor Called
+Destructor Called
+Destructor Called
+Destructor Called
+Destructor Called
+*/
